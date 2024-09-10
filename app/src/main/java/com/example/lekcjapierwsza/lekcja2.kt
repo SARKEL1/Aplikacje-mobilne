3be2d5804c0b037954da4e417de24afba137c350
Egzamin <bartlomiej.lapinski@infotech.edu.pl>
2024-09-10 09:43:02 +0200

    lekcjapierwsza

--- app/src/main/java/com/example/lekcjapierwsza/lekcja2.kt
+++ app/src/main/java/com/example/lekcjapierwsza/lekcja2.kt
@@ -1,6 +1,19 @@
 package com.example.lekcjapierwsza
 
-fun main(){
 
-    print("hello")
+fun main() {
+
+
+    var napis = readLine() ?: ""
+    var dlugosc = napis.length
+    println("Dlugosc napisu $dlugosc")
+
+    print("pomidor czy truskawka")
+    val owoc = readLine()
+    when (owoc) {
+        "pomidor" -> print("pomidor")
+        "truskawka" -> print("truskawka")
+        else -> print("To nie jest ani truskawka ani pomidor")
+    }
 }
+
